49a2de85d3372875998655efbdaff0d66cd2fdee
Kiyozumi Obuchi <kiyozumi.obuchi@gmail.com>
2024-09-18 18:00:03 +0900

    AC abc345 A

---
+++ contest/abc345/a/main.py
@@ -0,0 +1,11 @@
+S = input()
+
+if S[0] != "<" or S[-1] != ">":
+    print("No")
+    exit()
+
+for i in range(1, len(S) - 1):
+    if S[i] != "=":
+        print("No")
+        exit()
+print("Yes")
